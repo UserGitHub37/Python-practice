e6c3c272e7885b6dce6c934166f94dbf96b96a57
UserGitHub37 <bum37@mail.ru>
2025-03-05 12:30:32 +0300

    ауfeatfamake bubble sorting a function

--- 006_bubble-sort.py
+++ 006_bubble-sort.py
@@ -1,30 +1,36 @@
-#li = [5, 2, 1, 6, 4, 9, 5, 4, 3, 8, 10, 7, 1, 3]
-li = [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-# li = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
-print(li, "Не отсортированный список")
+list_1 = [5, 2, 1, 6, 4, 9, 5, 4, 3, 8, 10, 7, 1, 3]
+list_2 = [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+list_3 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
 
-listLength = len(li)
+def sort_bubble(list):
+    print(f"\n{list} Не отсортированный список")
 
-j = 0
-iterationCounter = 0
-changeCounter = 0
-while j < listLength - 1:
-    listIsChanged = False
-    i = 0
-    while i < listLength - 1:
-        iterationCounter += 1
-        num1 = li[i]
-        num2 = li[i + 1]
-        if num1 > num2:
-            changeCounter += 1
-            li[i] = num2
-            li[i + 1] = num1
-            listIsChanged = True
-        i += 1
-    if listIsChanged == False:
-        break
-    j += 1
+    list_length = len(list)
+    j = 0
+    iteration_counter = 0
+    change_counter = 0
 
-print(li, "Отсортированный список")
-print("Количество итераций", iterationCounter)
-print("Количество изменений списка:", changeCounter)
+    while j < list_length - 1:
+        listIs_changed = False
+        i = 0
+        while i < list_length - 1:
+            iteration_counter += 1
+            num_1 = list[i]
+            num_2 = list[i + 1]
+            if num_1 > num_2:
+                change_counter += 1
+                list[i] = num_2
+                list[i + 1] = num_1
+                listIs_changed = True
+            i += 1
+        if listIs_changed == False:
+            break
+        j += 1
+
+    print(list, "Отсортированный список")
+    print("Количество итераций:", iteration_counter)
+    print("Количество изменений списка:", change_counter, "\n")
+
+sort_bubble(list_1)
+sort_bubble(list_2)
+sort_bubble(list_3)
